e55c5774b5c1ab61f9f479968e16cf705726c81b
Mariano Rivera <mariano.rivera.castillo@gmail.com>
2021-04-16 11:36:09 -0600

    V1.1.0 Login & NuevaCuenta Created

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    // STATE PARA INICIO DE SESION
+    const [usuario, setUsuario] = useState({
+        email: '',
+        password: ''
+    });
+
+    // DESTRUCTURING STATE
+    const { email, password } = usuario;
+
+    // CADA QUE EL USUARIO ESCRIBA
+    const onChange = e => {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // CUANDO EL USUARIO PRESIONA EL BOTON
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // VALIDAR CAMPOS VACIOS
+
+        // PASARLO AL ACTION
+
+    }
+
     return (
-        <h1>Desde Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesión</h1>
+
+                <form onSubmit={onSubmit}>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tú Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tú Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Iniciar Sesion"
+                        />
+                    </div>
+                </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">
+                    Crear Nueva Cuenta
+                </Link>
+            </div>
+        </div>
     );
 }
 
--- src/components/auth/NuevaCuenta.js
+++ src/components/auth/NuevaCuenta.js
@@ -1,8 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const NuevaCuenta = () => {
+
+    // STATE PARA NUEVA CUENTA
+    const [usuario, setUsuario] = useState({
+        nombre: '',
+        email: '',
+        password: '',
+        confirmar: ''
+    });
+
+    // DESTRUCTURING STATE
+    const { nombre, email, password, confirmar } = usuario;
+
+    // CADA QUE EL USUARIO ESCRIBA
+    const onChange = e => {
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // CUANDO EL USUARIO PRESIONA EL BOTON
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // VALIDAR CAMPOS VACIOS
+
+        // PASSOWORD MINIMO 7 CARACTERES
+
+        // LOS 2 PASSWORD SEAN IGUALES
+
+        // PASARLO AL ACTION
+
+    }
+
     return (
-        <h1>Desde NuevaCuenta</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Crear una Nueva Cuenta</h1>
+
+                <form onSubmit={onSubmit}>
+                    <div className="campo-form">
+                        <label htmlFor="nombre">Nombre</label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            placeholder="Tú Nombre"
+                            value={nombre}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Tú Email"
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Tú Password"
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className="campo-form">
+                        <label htmlFor="confirmar">Confirmar Password</label>
+                        <input
+                            type="password"
+                            id="confirmar"
+                            name="confirmar"
+                            placeholder="Repite tú Password"
+                            value={confirmar}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className="campo-form">
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Registrarme"
+                        />
+                    </div>
+                </form>
+                <Link to={'/'} className="enlace-cuenta">
+                    Iniciar Sesión
+                </Link>
+            </div>
+        </div>
     );
 }
 
