462cf03a3d2d83a7cf87643ac1e0749a092bc2e8
Mariano Rivera <mariano.rivera.castillo@gmail.com>
2021-04-21 13:28:09 -0600

    V1.4.0 Tareas Folder Completed

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const FormTarea = () => {
 
@@ -7,31 +8,98 @@ const FormTarea = () => {
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
 
+    // OBTENER EL STATE DE TAREA
+    const tareasContext = useContext(tareaContext);
+    const { tareaseleccionada, errortarea, obtenerTareas,
+        agregarTarea, validarTarea, actualizarTarea } = tareasContext;
+
+    // DETECTA SI HAY UNA TAREA SELECCIONADA
+    useEffect(() => {
+        if (tareaseleccionada !== null) {
+            setTarea(tareaseleccionada);
+        } else {
+            setTarea({
+                nombre: ''
+            });
+        }
+    }, [tareaseleccionada]);
+
+    // DEFINIR STATE FORMULARIO
+    const [tarea, setTarea] = useState({
+        nombre: ''
+    })
+
+    // DESTRUCTURING STATE
+    const { nombre } = tarea;
+
     // SI NO HAY PROYECTO SELECCIONADO
     if (!proyecto) return null;
 
     // ARRAY DESTRUCTURING - EXTRAER PROYECTO ACTUAL
     const [proyectoActual] = proyecto;
 
+    // LEER LOS VALORES DEL FORMULARIO
+    const handleChange = e => {
+        setTarea({
+            ...tarea,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        // VALIDAR LA TAREA
+        if (nombre.trim() === '') {
+            validarTarea();
+            return;
+        }
+
+        // REVISAR SI VA A EDITAR O AGREGAR UNA TAREA
+        if (tareaseleccionada === null) {
+            // AGREGAR LA NUEVA TAREA AL STATE
+            tarea.proyectoId = proyectoActual.id;
+            tarea.estado = false;
+            agregarTarea(tarea);
+        } else {
+            // ACTUALIZAR TAREA EXISTENTE
+            actualizarTarea(tarea);
+        }
+
+        // OBTENER Y FILTRAR LAS TAREAS DEL PROYECTO ACTUAL
+        obtenerTareas(proyectoActual.id);
+
+        // REINICIAR EL FORM
+        setTarea({
+            nombre: ''
+        })
+    }
+
     return (
         <div className="formulario">
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="contenedor-input">
                     <input
                         type="text"
                         className="input-text"
                         placeholder="Nombre Tarea.."
                         name="nombre"
+                        value={nombre}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="contenedor-input">
                     <input
                         type="submit"
                         className="btn btn-primario btn-submit btn-block"
-                        value="Agregar Tarea"
+                        value={tareaseleccionada ? 'Editar Tarea' : 'Agregar Tarea'}
                     />
                 </div>
             </form>
+            {errortarea
+                ? <p className="mensaje error">El Nombre de la Tarea es Obligatorio</p>
+                : null
+            }
         </div>
     );
 }
